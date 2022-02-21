aa8130f422f0478f62ef39180cc25f1a86cbc198
Stephen Cashin <cashins@tcd.ie>
2022-02-21 10:24:38 +0000

    Added Response.js

--- Front-End/ui/src/App.js
+++ Front-End/ui/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './Components/Button.js'
+import Response from './Components/Response.js'
 
 function App() {
   return (
     <div className="Container">
       <h1>Hello World</h1>
       <Button color='blue' text='Run Query'/>
+      <Response text = 'We and our partners store
+        and/or access information on a device, such
+        as cookies and process personal data, such
+        as unique identifiers and standard information
+        sent by a device for personalised ads and content,
+        ad and content measurement, and audience insights,
+        as well as to develop and improve products.'/>
     </div>
   );
 }
---
+++ Front-End/ui/src/Components/Response.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Response = ({text}) => {
+    return (
+        <p>{text}</p>
+    )
+}
+
+export default Response;
