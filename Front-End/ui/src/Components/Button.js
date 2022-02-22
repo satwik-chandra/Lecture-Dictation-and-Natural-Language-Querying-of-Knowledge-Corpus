410a1f6ba57307753cb14ba687cee892c5f20784
Stephen Cashin <cashins@tcd.ie>
2022-02-22 13:21:03 +0000

    Added Dummy backend

--- Front-End/ui/src/Components/Button.js
+++ Front-End/ui/src/Components/Button.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Response from './Response.js'
 
 export const Button = ({color, text, resp}) => {
@@ -7,9 +7,30 @@ export const Button = ({color, text, resp}) => {
     const [response, setResponse] = useState(resp)
 
     const onClick = (e) => {
-        setResponse('Button Has been Pressed');
+        //const respFromServer = fetchResp();
+        setResponse("You pressed the Button");
+        //console.log(respFromServer);
         console.log("You pressed the Button");
     }
+
+    // useEffect(() => {
+    //    const getResp = async () => {
+    //        const respFromServer = await fetchResp()
+    //        //setResponse(respFromServer[0].resp)
+    //    }
+
+    //     fetchResp()
+
+    // }, [])
+
+//    const fetchResp = async () => {
+//         const res = await fetch('http://localhost:5000/responses')
+//         const data = await res.json()
+
+//         //console.log(data)
+//          return data
+//     }
+
     return (
     <div>
         <button onClick={onClick} 
