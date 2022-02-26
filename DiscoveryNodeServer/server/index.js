0e7971e91f73030d7a64c58b651714442680c2a5
Satwik Chandra <satwik.chandra2001@gmail.com>
2022-02-26 14:48:37 +0000

    Api endoint made for the natural language query, auto complete suggestions still pending

--- DiscoveryNodeServer/server/index.js
+++ DiscoveryNodeServer/server/index.js
@@ -2,14 +2,7 @@
 
 const express = require("express");
 
-let Discovery_url = "https://api.eu-gb.discovery.watson.cloud.ibm.com/instances/ee277a06-704d-45b4-a204-9dff9216fc9a";
-let API_key = "umf4q-OA2qkJvgWZhkeLe8e0Q8vsCufrJgmEu0ocbfxu";
-
-let collection_id = "665e9d8b-ff19-4ad8-93fd-eddee01884fe";
-let configuration_id = "2883717b-0f6c-4aa5-852b-7c9f6c3d20b2";
-let environment_id = "b2143037-e1b7-4a25-9273-da6a00d6beb1";
-
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -19,51 +12,44 @@ const { IamAuthenticator } = require('ibm-watson/auth');
 const discovery = new DiscoveryV1({
   version: '2019-04-30',
   authenticator: new IamAuthenticator({
-    apikey: API_key,
+    apikey: 'umf4q-OA2qkJvgWZhkeLe8e0Q8vsCufrJgmEu0ocbfxu',
   }),
-  serviceUrl: Discovery_url,
+  serviceUrl: 'https://api.eu-gb.discovery.watson.cloud.ibm.com/instances/ee277a06-704d-45b4-a204-9dff9216fc9a',
 });
 
-//function to get send a query and get result from discovery api
-async function get_result(query) {
-  let result = await discovery.query({
-    environmentId: environment_id,
-    collectionId: collection_id,
-    query: query,
-    passages: true,
-    count: 5,
-    return: 'highlight,passages',
-  }).catch(err => {
-    console.log(err);
-  });
-  return result;
-}
+const queryParams = {
+  environmentId: 'system',
+  collectionId: 'news-en',
+};
 
-//function to get auto complete suggestions for a prefix using getAutocompletions()
-async function get_autocomplete(prefix) {
-  let result = await discovery.getAutocompletions({
-    environmentId: environment_id,
-    collectionId: collection_id,
-    prefix: prefix,
-    count: 5,
-  }).catch(err => {
-    console.log(err);
-  });
-  return result;
+
+//function that takes in a string as natural language query and then send the natural language query to the discovery service and returns the response
+async function getResponse(query) {
+  const response = await discovery.query(queryParams,query);
+  return response;
 }
 
 
+//make an api endpoint that uses getResponse function to get the response for a natural language query
+app.get("/api/discovery/:query", async (req, res) => {
+  const query = req.params.query;
+  const response = await getResponse(query);
+  res.json(response);
+});
+
+//make another api endpoint that returns the autocomplete suggestions for a prefix using the discovery service  (this is the same as the autocomplete suggestions in the discovery service)
+app.get("/api/autocomplete/:prefix", async (req, res) => {
+  const prefix = req.params.prefix;
+  const response = await discovery.getAutocompletion(queryParams,prefix);
+  res.json(response);
+});
 
 
-app.get("/api", (req, res) => {
+
+/* app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
-  });
-  
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+  }); */
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
