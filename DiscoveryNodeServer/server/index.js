679672bf453ab57d8def20a56b95bc9290effcdb
Satwik Chandra <satwik.chandra2001@gmail.com>
2022-02-27 17:20:32 +0000

    Merge remote-tracking branch 'origin/main' into Satwik

--- DiscoveryNodeServer/server/index.js
+++ DiscoveryNodeServer/server/index.js
@@ -10,45 +10,55 @@ const DiscoveryV1 = require('ibm-watson/discovery/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 const discovery = new DiscoveryV1({
-  version: '2019-04-30',
+  version: '2018-12-03',
   authenticator: new IamAuthenticator({
     apikey: 'umf4q-OA2qkJvgWZhkeLe8e0Q8vsCufrJgmEu0ocbfxu',
   }),
   serviceUrl: 'https://api.eu-gb.discovery.watson.cloud.ibm.com/instances/ee277a06-704d-45b4-a204-9dff9216fc9a',
 });
 
-const queryParams = {
+var queryParams = {
   environmentId: 'system',
   collectionId: 'news-en',
+  naturalLanguageQuery: "",  //this is the query that is passed to the discovery service
+  passages: true,
+  count: 3,
+  passagesCharacters: 200,
+  deduplicate: false,
 };
+
+var auto_complete_prams = {
+  environmentId: 'system',
+  collectionId: 'news-en',
+  count: 3,
+  prefix: "",  //this is the query that is passed to the discovery service
+};
+
 
 
 //function that takes in a string as natural language query and then send the natural language query to the discovery service and returns the response
 async function getResponse(query) {
-  const response = await discovery.query(queryParams,query);
+  queryParams.naturalLanguageQuery = query;
+  const response = await discovery.query(queryParams);
   return response;
 }
 
 
-//make an api endpoint that uses getResponse function to get the response for a natural language query
-app.get("/api/discovery/:query", async (req, res) => {
+//API endpoint that uses getResponse function to get the response for a natural language query
+app.get("/api/discovery/query/:query", async (req, res) => {
   const query = req.params.query;
   const response = await getResponse(query);
   res.json(response);
 });
 
-//make another api endpoint that returns the autocomplete suggestions for a prefix using the discovery service  (this is the same as the autocomplete suggestions in the discovery service)
-app.get("/api/autocomplete/:prefix", async (req, res) => {
-  const prefix = req.params.prefix;
-  const response = await discovery.getAutocompletion(queryParams,prefix);
+
+//API endpoint that takes in a string as prefix and then send the prefix to the discovery service and returns the response(NOT WORKING)
+app.get("/api/autocomplete/prefix/:prefix", async (req, res) => {
+  auto_complete_prams.prefix = req.params.prefix;
+  const response = await discovery.getAutocompletion(auto_complete_prams);
   res.json(response);
 });
 
-
-
-/* app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  }); */
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
