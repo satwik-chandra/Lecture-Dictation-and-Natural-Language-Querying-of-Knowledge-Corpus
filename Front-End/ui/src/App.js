539de2811a4d746e4b66eb35b14d89fad9c5c3e3
Stephen Cashin <cashins@tcd.ie>
2022-02-28 16:27:07 +0000

    second half of the prev commit

--- Front-End/ui/src/App.js
+++ Front-End/ui/src/App.js
@@ -10,39 +10,22 @@ function App() {
   const [responses, setResponses] = useState([])
 
   useEffect(() => {
-      //  const getResp = async () => {
-      //  const respFromServer = await fetchResp()
-      //  setResponses(respFromServer)
-      //  }
-      const a = fetchResp()
-      //getResp()
+       const getResp = async () => {
+       const respFromServer = await fetchResp()
+       setResponses(respFromServer)
+       }
+
+      getResp()
 
     }, [])
 
   const fetchResp = async () => {
       const res = await fetch('Access-Control-Allow-Origin', 'http://localhost:3001/api/discovery/query/google-and-ai')
-      //const data = await res.json()
-      console.log(res)
-      console.log(typeof res)
-      return res
+      const data = await res.json()
+      console.log(data)
+      console.log(typeof data)
+      return data
   }
-
- // const fetchResp = async () => {
-    // const res = await fetch('http://localhost:3001/api/discovery/query/google-and-ai', { 
-    //   mode: 'no-cors'
-    // });
-    // const data = await res.json()
-
-//     const res = fetch('http://localhost:3001/api/discovery/query/google-and-ai', {
-//       mode: 'no-cors',
-//     }).then(function(e_) { 
-//     const data = await res.json()
-//     });
-
-//     console.log(data)
-//     console.log(typeof data)
-//     return data
-// }
 
   return (
     <div className="Container">
