620398c49ee1a57aea1f7feaeb2168989b188a2b
Stephen Cashin <cashins@tcd.ie>
2022-03-11 13:17:07 +0000

    added a video player in lecture screen

--- Front-End/ui/src/App.js
+++ Front-End/ui/src/App.js
@@ -4,7 +4,10 @@ import Response from './Components/Response.js'
 import SearchBar from './Components/SearchBar.js'
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-  
+import ReactPlayer from 'react-player'
+import video from './r.mp4'
+
+
   const HomePage = () => {
     return (
           <div className="Container">
@@ -17,10 +20,27 @@ import { useState, useEffect } from 'react'
     
   //export default HomePage;
   const LecturePage = () => {
+
     return (
       <div className="Container">
         <h1>Lecture Page</h1>
         <Link to='/'>Back</Link>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+         }}
+        className='player-wrapper'>
+            <ReactPlayer
+            className='react-player'
+            url= {video}
+            width='60%'
+            height='60%'
+            controls = {true}
+
+            />
+        </div>
+        
       </div>
     )
   };
